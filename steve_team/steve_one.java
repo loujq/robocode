558cb803c44588ae1e06009425bc4c554648f372
loujq <loujq@sohu.com>
2018-06-29 16:45:30 +0800

    modify

--- steve_team/steve_one.java
+++ steve_team/steve_one.java
@@ -1,12 +1,12 @@
 /**
- * Copyright (c) 2001-2017 Mathew A. Nelson and Robocode contributors
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://robocode.sourceforge.net/license/epl-v10.html
+ * Copyright (c) 2018 Steve Lou
+ * All rights reserved. This program and the accompanying 
+ * materials are made available under the terms of the Eclipse 
+ * Public License v1.0 which accompanies this distribution, and 
+ * is available at 
+ * http://robocode.sourceforge.net/license/epl-v10.html 
  */
-package sample;
-
+package steve_team;
 
 import robocode.DeathEvent;
 import robocode.Robot;
@@ -14,7 +14,6 @@ import robocode.ScannedRobotEvent;
 import static robocode.util.Utils.normalRelativeAngleDegrees;
 
 import java.awt.*;
-
 
 /**
  * Corners - a sample robot by Mathew Nelson.
